9f7703a8406e76972ccae9f71432622b97655493
Iman Toumi <reyan.toumi@gmail.com>
2018-02-07 21:14:12 +0400

    Reverted again

---
+++ Assets/Scripts/DestroyAfterHitCount.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DestroyAfterHitCount : MonoBehaviour {
+
+    // Delete this script after and put it in ball script
+    public int maxHitCount = 4;
+
+    private int collisionCount = 0;
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        collisionCount++;
+        if (collisionCount >= maxHitCount) {
+            Destroy(gameObject);
+        }
+    }
+}
